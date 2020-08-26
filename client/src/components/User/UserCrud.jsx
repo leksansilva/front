9c4ca635ce653146197c21d0492d335d9a3d9015
leksansilva <69767375+leksansilva@users.noreply.github.com>
2020-08-26 20:55:53 -0300

    modificações para receber o back

--- client/src/components/User/UserCrud.jsx
+++ client/src/components/User/UserCrud.jsx
@@ -8,50 +8,47 @@ export default class UserCrud extends Component{
   render(){
     return (
       <Main icon="users" title="Cadastro" {...headerProps}>
-      <fom>
+   <form>
+ 
+  <div className="form-group">
+    <label for="inputAddress">Nome completo</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="Nome completo"></input>
+  </div>
+  <div className="form-row">
+    <div className="form-group col-md-6">
+      <label for="inputEmail4">Email</label>
+      <input type="email" className="form-control" id="inputEmail4" placeholder="Email"></input>
+    </div>
+    <div className="form-group col-md-6">
+      <label for="inputPassword4">Telefone</label>
+      <input type="password" className="form-control" id="inputPassword4" placeholder="Telefone"></input>
+    </div>
+    <div className="form-group col-md-6">
+      <label for="inputEmail4">RG</label>
+      <input type="email" className="form-control" id="inputEmail4" placeholder="RG"></input>
+    </div>
+    <div className="form-group col-md-6">
+      <label for="inputPassword4">CPF</label>
+      <input type="password" className="form-control" id="inputPassword4" placeholder="CPF"></input>
+    </div>
+
+    <div className="form-group col-md-6">
+      <label for="inputEmail4">Senha</label>
+      <input type="email" className="form-control" id="inputEmail4" placeholder="Senha"></input>
+    </div>
+    <div className="form-group col-md-6">
+      <label for="inputPassword4">Confirme a senha</label>
+      <input type="password" className="form-control" id="inputPassword4" placeholder="confirme a senha"></input>
+    </div>
+  </div>
   
-     <div className="form-group">
-       <label for="inputAddress">Nome completo</label>
-       <input type="text" class="form-control" id="inputAddress" placeholder="Nome completo"></input>
-     </div>
-     <div className="form-row">
-       <div className="form-group col-md-6">
-         <label for="inputEmail4">Email</label>
-         <input type="email" className="form-control" id="inputEmail4" placeholder="Email"></input>
-       </div>
-       <div className="form-group col-md-6">
-         <label for="inputPassword4">Telefone</label>
-         <input type="password" className="form-control" id="inputPassword4" placeholder="Telefone"></input>
-       </div>
-       <div className="form-group col-md-6">
-         <label for="inputEmail4">RG</label>
-         <input type="email" className="form-control" id="inputEmail4" placeholder="RG"></input>
-       </div>
-       <div className="form-group col-md-6">
-         <label for="inputPassword4">CPF</label>
-         <input type="password" className="form-control" id="inputPassword4" placeholder="CPF"></input>
-       </div>
-    
-       <div className="form-group col-md-6">
-         <label for="inputEmail4">Senha</label>
-         <input type="email" className="form-control" id="inputEmail4" placeholder="Senha"></input>
-       </div>
-       <div className="form-group col-md-6">
-         <label for="inputPassword4">Confirme a senha</label>
-         <input type="password" className="form-control" id="inputPassword4" placeholder="confirme a senha"></input>
-       </div>
-     </div>
-     
-     
-     
-     <button type="button" className="btn btn-warning">Cadastre-se</button>
-     
-    </form>
-    
-         </Main>
-     )
-    }
+  
+  
+  <button type="button" class="btn btn-primary">Cadastre-se</button>
+  
+</form>
+
+      </Main>
+    );
   }
-
-
-  
+}
